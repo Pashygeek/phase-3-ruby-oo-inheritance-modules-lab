c98a0a35e1517e3ee48d157595fb91dbe8494316
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-28 14:22:41 +0300

    Completed assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require_relative 'song'
 
 class Artist
   attr_accessor :name
@@ -40,3 +41,28 @@ class Artist
     name.downcase.gsub(' ', '-')
   end
 end
+
+# output
+song1 = Song.new
+song1.name = "Song 1"
+
+song2 = Song.new
+song2.name = "Song 2"
+
+artist = Artist.new
+artist.name = "Artist"
+
+artist.add_song(song1)
+artist.add_song(song2)
+
+puts "Total songs: #{Song.count}"         # Output: Total songs: 2
+puts "Total artists: #{Artist.count}"     # Output: Total artists: 1
+
+found_song = Song.find_by_name("Song 2")
+puts "Found song: #{found_song.name}"     # Output: Found song: Song 2
+
+found_artist = Artist.find_by_name("Artist")
+puts "Found artist: #{found_artist.name}" # Output: Found artist: Artist
+
+puts "Artist songs: #{found_artist.songs.map(&:name)}" # Output: Artist songs: [Song 1, Song 2]
+
